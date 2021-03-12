fe646fa07f9eae3d3ffe73cb2d9423b25c9e70ca
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-03-11 20:53:48 -0500

    added boilerplate code

---
+++ src/components/TableRow.js
@@ -0,0 +1,13 @@
+import TableCell from "./components/TableCell";
+
+function TableRow(props) {
+  let cells = [];
+  
+  for (let i = 0; i < props.numCols; i++) {
+    cells.push(<TableCell handleApplyColor={props.handleApplyColor} />)
+  }
+  
+  return <tr>{cells}</tr>
+}
+
+export default TableRow;
