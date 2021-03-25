ffdf49c0e48dac20b14c0ebfe9a2de272c00e184
Joannef <joannef3188@gmail.com>
2021-03-25 17:10:41 -0400

    Complete FillU

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import TableRow from "./TableRow";
+import ReactDOM from 'react-dom';
 
 class Table extends Component {
   constructor() {
@@ -9,6 +10,9 @@ class Table extends Component {
       numCols: 1,
       selectedColor: "red"
     }
+    
+    this.grid = React.createRef();
+
   }
 
   addRow = () => {
@@ -25,14 +29,54 @@ class Table extends Component {
 
   removeRow = () => {
     this.setState(state => {
-      return { numRows: state.numRows <= 1 ? 1: state.numRows - 1}
-
+      return { numRows: state.numRows <= 1 ? 1: state.numRows - 1} //if numRows is greater than or equal to 1
     });
   }
 
   removeCol = () => {
     this.setState(state => {
       return {numCols: state.numCols <= 1 ? 1: state.numCols - 1}
+    });
+  }
+
+  fillU = () => {
+    let table = ReactDOM.findDOMNode(this.grid.current).childNodes;
+
+    table.forEach(row => {
+      for(let i = 0; i < this.state.numCols; i++) {
+        let backgroundColor = row.childNodes[i].style.backgroundColor
+        
+        if(backgroundColor === "")
+          row.childNodes[i].style.backgroundColor = this.state.selectedColor
+          
+      }
+    });
+  }
+
+  fillAll = () => {
+    let row = document.getElementsByTagName("tr")
+    for(const element of row)
+    {
+      let col = element.getElementsByTagName("td");
+      for(let j = 0; j < this.state.numCols; j++)
+      {
+        col[j].style.background = this.state.selectedColor;
+      }
+    }
+  }
+
+  clearAll = () => {
+    let table = ReactDOM.findDOMNode(this.grid.current).childNodes; //tr = nodes, td = childNodes
+    //console.log(table)
+
+    table.forEach(row => { // 1st loop
+      for(let i = 0; i < this.state.numCols; i++) { // 2nd loop
+        let backgroundColor = row.childNodes[i].style.backgroundColor
+        
+        if(backgroundColor !== "")
+          row.childNodes[i].style.backgroundColor = "white"
+          
+      }
     });
   }
 
@@ -51,22 +95,25 @@ class Table extends Component {
       rows.push(<TableRow numCols={this.state.numCols} handleApplyColor={this.handleApplyColor} />);
     }
 
-    //TODO: Add onclick functions to the buttons 
     return (
       <div>
         <button onClick={this.addRow}>Add Row</button>
         <button onClick={this.addColumn}>Add Column</button>
         <button onClick={this.removeRow}>Remove Row</button>
         <button onClick={this.removeCol}>Remove Col</button>
-        <button>Fill All Uncolored</button>
-        <button>Fill All</button>
-        <button>Clear</button>
+        <button onClick={this.fillU}>Fill All Uncolored</button>
+        <button onClick={this.fillAll}>Fill All</button>
+        <button onClick={this.clearAll}>Clear</button>
         <select onChange={this.handleColorChange}>
-          <option value="red">red</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="purple">Purple</option>
         </select>
-        <table>
+        <table ref={this.grid}>
           {rows}
         </table>
       </div>
